045fe5765ecb852c27fa6046a0df411072f67f6d
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-26 14:22:49 +0200

    clone v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-""" index file """
+"""
+Views index, contains status and stat endpoints.
+"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
-def status_check():
-    """ returns status OK """
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
+
+
+@app_views.route("/status")
+def status():
+    """Status of my API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def get_stats():
-    """ returns count of all classes' objects """
-    return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    })
+@app_views.route("/stats")
+def stats():
+    """Retrieve the number of each objects by type"""
+    statistics = {}
+
+    for key, value in classes.items():
+        statistics[key] = storage.count(value)
+
+    return jsonify(statistics)
