e78bed336f5fd3062dba7b8cd675fb0fd9b5d1a8
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-26 20:06:47 +0200

    clone v3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,19 @@ class FileStorage:
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
+    def get(self, cls, id):
+        """ Return one object or `None` if not found """
+        result = self.all(cls)
+        for objs in result.values():
+            if objs.id == id:
+                return 
+        return None
+    
+    def count(self, cls=None):
+        """returns count of object in the storage"""
+        result = self.all(cls)
+        return len(result)
+
     def all(self, cls=None):
         """returns the dictionary __objects"""
         if cls is not None:
@@ -56,7 +69,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -69,17 +82,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """ gets an object by id """
-        result = self.all(cls)
-        for item in result.values():
-            if item.id == id:
-                return item
-        return None
-
-    def count(self, cls=None):
-        """Return the number of objects in storage"""
-        if cls is not None:
-            return len(self.all(cls))
-        return len(self.__objects)
