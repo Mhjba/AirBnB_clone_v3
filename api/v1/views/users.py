166635ed5f6fb3ec553cb9bc2a36b4bbb98c16d5
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-27 17:22:43 +0200

    clonr v3

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,19 +6,20 @@ from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify, request, abort
+import models
 
 
 @app_views.route('/users', methods=['GET'])
 def list_users():
     """ Get all users """
-    all_users = [obj.to_dict() for obj in storage.all("User").values()]
+    all_users = [obj.to_dict() for obj in models.storage.all("User").values()]
     return jsonify(all_users)
 
 
 @app_views.route("/users/<user_id>", methods=['GET'])
 def get_user(user_id):
     """ Get User id """
-    all_user = storage.get(User, user_id)
+    all_user = models.storage.get(User, user_id)
     if not all_user:
         abort(404)
     return jsonify(all_user.to_dict())
@@ -27,11 +28,11 @@ def get_user(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     """ Deletes a User id """
-    all_user = storage.get(User, user_id)
+    all_user = models.storage.get(User, user_id)
     if all_user is None:
         abort(404)
-    storage.delete(all_user)
-    storage.save()
+    models.storage.delete(all_user)
+    models.storage.save()
     return jsonify({}), 200
 
 
@@ -46,15 +47,15 @@ def create_user():
     if "email" not in all_user:
         abort(400, "Missing email")
     new_user = User(**all_user)
-    storage.new(new_user)
-    storage.save()
+    models.storage.new(new_user)
+    models.storage.save()
     return new_user.to_dict(), 201
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def updates_user(user_id):
     """ Update an user id """
-    all_user = storage.get(User, user_id)
+    all_user = models.storage.get(User, user_id)
     if not all_user:
         abort(404)
     if not request.is_json:
@@ -64,5 +65,5 @@ def updates_user(user_id):
     for objs, v in user_obj.items():
         if objs != "id" and objs != "updated_at" and objs != "created_at":
             setattr(all_user, objs, v)
-    storage.save()
+    models.storage.save()
     return all_user.to_dict(), 200
