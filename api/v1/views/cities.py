9bc6c51514e9edd3b2af72b7b2ab5c34e1b291ca
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-27 19:04:54 +0200

    clone v3

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -46,23 +46,23 @@ def delete_city_by_id(city_id):
     return jsonify({}), 200
 
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"])
+@app_views.route('/states/<state_id>/cities/', methods=['POST'])
 def create_city(state_id):
-    """
-    Create a city by state id
-    """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
+    '''Creates a City'''
     if not request.get_json():
-        abort(400, "Not a JSON")
-    if "name" not in request.get_json():
-        abort(400, "Missing name")
-
-    city = City(state_id=state_id, **request.get_json())
-    city.save()
-
-    return jsonify(city.to_dict()), 201
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
+        abort(404)
+    cities = []
+    new_city = City(name=request.json['name'], state_id=state_id)
+    storage.new(new_city)
+    storage.save()
+    cities.append(new_city.to_dict())
+    return jsonify(cities[0]), 201
 
 
 @app_views.route("/cities/<string:city_id>", methods=['PUT'])
