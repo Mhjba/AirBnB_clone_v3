f03fa473fd54f0339abea741c94af6552c3bb607
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-27 12:47:32 +0200

    clone v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,6 @@
 """ index file """
 from api.v1.views import app_views
 from flask import jsonify
-import models
 from models import storage
 
 
@@ -12,14 +11,14 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/status")
-def stats():
+@app_views.route("/stats", strict_slashes=False)
+def get_stats():
     """ returns count of all classes' objects """
     return jsonify({
-        "amenities": models.storage.count("Amenity"),
-        "cities": models.storage.count("City"),
-        "places": models.storage.count("Place"),
-        "reviews": models.storage.count("Review"),
-        "states": models.storage.count("State"),
-        "users": models.storage.count("User")
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     })
