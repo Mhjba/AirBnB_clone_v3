932c4f5d7b3a8968d6286ec9809904194900d843
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-28 14:33:32 +0200

    clone v3

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -12,26 +12,26 @@ import models
 @app_views.route('/users', methods=['GET'])
 def list_users():
     """ Get all users """
-    lt_users = [obj.to_dict() for obj in models.storage.all("User").values()]
-    return jsonify(lt_users)
+    all_users = [obj.to_dict() for obj in models.storage.all("User").values()]
+    return jsonify(all_users)
 
 
 @app_views.route("/users/<user_id>", methods=['GET'])
 def get_user(user_id):
-    """ Get User """
-    gt_user = models.storage.get(User, user_id)
-    if not gt_user:
+    """ Get User id """
+    all_user = models.storage.get(User, user_id)
+    if not all_user:
         abort(404)
-    return jsonify(gt_user.to_dict())
+    return jsonify(all_user.to_dict())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    """ Deletes User """
-    dl_user = models.storage.get(User, user_id)
-    if dl_user is None:
+    """ Deletes a User id """
+    all_user = models.storage.get(User, user_id)
+    if all_user is None:
         abort(404)
-    models.storage.delete(dl_user)
+    models.storage.delete(all_user)
     models.storage.save()
     return jsonify({}), 200
 
@@ -41,12 +41,12 @@ def create_user():
     """ Creates a User """
     if not request.is_json:
         abort(400, "Not a JSON")
-    cr_user = request.get_json()
+    all_user = request.get_json()
     if "password" not in all_user:
         abort(400, "Missing password")
-    if "email" not in cr_user:
+    if "email" not in all_user:
         abort(400, "Missing email")
-    new_user = User(**cr_user)
+    new_user = User(**all_user)
     models.storage.new(new_user)
     models.storage.save()
     return new_user.to_dict(), 201
@@ -54,9 +54,9 @@ def create_user():
 
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def updates_user(user_id):
-    """ Update user """
-    up_user = models.storage.get(User, user_id)
-    if not up_user:
+    """ Update an user id """
+    all_user = models.storage.get(User, user_id)
+    if not all_user:
         abort(404)
     if not request.is_json:
         abort(400, "Not a JSON")
@@ -64,6 +64,6 @@ def updates_user(user_id):
     user_obj = request.get_json()
     for objs, v in user_obj.items():
         if objs != "id" and objs != "updated_at" and objs != "created_at":
-            setattr(up_user, objs, v)
+            setattr(all_user, objs, v)
     models.storage.save()
-    return up_user.to_dict(), 200
+    return all_user.to_dict(), 200
