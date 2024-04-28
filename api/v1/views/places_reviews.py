14b867acc0c08d1968f34c2690b82ee618f93647
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-28 18:29:20 +0200

    clone v3

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -39,3 +39,57 @@ def delete_review_by_id(review_id):
     storage.delete(req_review)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route("/places/<place_id>/reviews", methods=['POST'],
+                 strict_slashes=False)
+def create_new_review(place_id):
+    """
+    Create a new review
+    """
+    req_place = storage.get("Place", place_id)
+    if not req_place:
+        abort(404)
+    res_json = request.get_json()
+    if res_json is None:
+        abort(400, 'Not a JSON')
+    if 'text' not in res_json:
+        abort(400, 'Missing text')
+    if 'user_id' not in res_json:
+        abort(400, 'Missing user_id')
+    get_u = storage.get("User", res_json['user_id'])
+    if not get_u:
+        abort(404)
+    req_review = Review(**res_json)
+    req_review.user_id = res_json['user_id']
+    req_review.place_id = place_id
+    req_review.save()
+    return jsonify(req_review.to_dict()), 201
+
+
+@app_views.route("/reviews/<review_id>", methods=['PUT'],
+                 strict_slashes=False)
+def update_review_by_id(review_id):
+    """
+    Update an review with the given id
+    """
+    req_review = storage.get("Review", review_id)
+    if not req_review:
+        abort(404)
+    res_json = request.get_json()
+    if res_json is None:
+        abort(400, 'Not a JSON')
+    if 'text' in res_json:
+        req_review.text = res_json['text']
+    if 'place_id' in res_json:
+        req_place = storage.get("Place", res_json['place_id'])
+        if not req_place:
+            abort(404)
+        req_review.place_id = res_json['place_id']
+    if 'user_id' in res_json:
+        get_u = storage.get("User", res_json['user_id'])
+        if not get_u:
+            abort(404)
+        req_review.user_id = res_json['user_id']
+    req_review.save()
+    return jsonify(req_review.to_dict()), 200
