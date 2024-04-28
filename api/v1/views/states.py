e9ba86f1f3c98032d258dff2f6b024849b0d851e
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-28 20:36:56 +0200

    clone v3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" state API """
+""" state """
 
 import json
 from models import storage
@@ -7,19 +7,20 @@ from models.state import State
 from api.v1.views import app_views
 from models.base_model import BaseModel
 from flask import Blueprint, jsonify, request, abort
+import models
 
 
 @app_views.route("/states", methods=["GET"])
 def all_states():
     """ get all states """
-    all_states = [obj.to_dict() for obj in storage.all("State").values()]
+    all_states = [obj.to_dict() for obj in models.storage.all("State").values()]
     return jsonify(all_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """ get state """
-    gt_state = storage.get(State, state_id)
+    gt_state = models.storage.get(State, state_id)
     if gt_state is None:
         return abort(404)
     return jsonify(gt_state.to_dict())
@@ -28,11 +29,11 @@ def get_state(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """ delete state """
-    dl_state = storage.get(State, state_id)
+    dl_state = models.storage.get(State, state_id)
     if dl_state is None:
         return abort(404)
-    storage.delete(dl_state)
-    storage.save()
+    models.storage.delete(dl_state)
+    models.storage.save()
     return jsonify({}), 200
 
 
@@ -45,15 +46,15 @@ def create_state():
     if "name" not in js:
         return abort(400, description="Missing name")
     cr_state = State(**js)
-    storage.new(cr_state)
-    storage.save()
+    models.storage.new(cr_state)
+    models.storage.save()
     return cr_state.to_dict(), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def updates_state(state_id):
     """ Updates state info """
-    up_state = storage.get(State, state_id)
+    up_state = models.storage.get(State, state_id)
 
     if not up_state:
         return abort(404)
@@ -63,5 +64,5 @@ def updates_state(state_id):
     for key, value in js.items():
         if key != "id" and key != "updated_at" and key != "created_at":
             setattr(up_state, key, value)
-    storage.save()
+    models.storage.save()
     return up_state.to_dict(), 200
