18c33f540973c3d7b625cb27597977c69d9a54cd
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-29 15:13:37 +0200

    clone v3

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -27,8 +27,7 @@ def get_review_by_id(review_id):
     return jsonify(req_review.to_dict())
 
 
-@app_views.route("/reviews/<review_id>", methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route("/reviews/<review_id>", methods=['DELETE'])
 def delete_review_by_id(review_id):
     """
     Delete a review with the given id
@@ -39,29 +38,3 @@ def delete_review_by_id(review_id):
     storage.delete(req_review)
     storage.save()
     return jsonify({}), 200
-
-
-@app_views.route("/places/<place_id>/reviews", methods=['POST'],
-                 strict_slashes=False)
-def create_new_review(place_id):
-    """
-    Create a new review
-    """
-    req_place = storage.get("Place", place_id)
-    if not req_place:
-        abort(404)
-    res_json = request.get_json()
-    if res_json is None:
-        abort(400, 'Not a JSON')
-    if 'text' not in res_json:
-        abort(400, 'Missing text')
-    if 'user_id' not in res_json:
-        abort(400, 'Missing user_id')
-    get_u = storage.get("User", res_json['user_id'])
-    if not get_u:
-        abort(404)
-    req_review = Review(**res_json)
-    req_review.user_id = res_json['user_id']
-    req_review.place_id = place_id
-    req_review.save()
-    return jsonify(req_review.to_dict()), 201
